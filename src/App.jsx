14e6ee9f28cc442c8805d603939d977c1d0b7dfb
Ali Mikdad <42510009@students.liu.edu.lb>
2026-08-09 23:24:27 +0300

    added separate pages for each link on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
 
-function App() {
+function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Technologies />
     </>
+  );
+}
+
+function TechnologiesPage() {
+  return <h1 style={{ padding: "80px 8%" }}>Technologies</h1>;
+}
+
+function FuturePage() {
+  return <h1 style={{ padding: "80px 8%" }}>Future of IT</h1>;
+}
+
+function AboutPage() {
+  return <h1 style={{ padding: "80px 8%" }}>About</h1>;
+}
+
+function ContactPage() {
+  return <h1 style={{ padding: "80px 8%" }}>Contact</h1>;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/technologies" element={<TechnologiesPage />} />
+        <Route path="/future" element={<FuturePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
