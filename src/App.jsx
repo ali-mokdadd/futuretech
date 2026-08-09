3df06f24539783faa37e0f150fcab41aaf241107
ahlaweb <ahlaweb1@gmail.com>
2026-08-09 22:56:04 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>FutureTech</h1>
+        <p>Exploring the technologies shaping our future.</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
