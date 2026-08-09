0a660e74f8b83b98f9668786855d8d1a7d3f6b15
Ali Mikdad <42510009@students.liu.edu.lb>
2026-08-09 22:56:04 +0300

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h2>FutureTech</h2>
+      </div>
+
+      <div>
+        <a href="#">Home</a>
+        <a href="#">Technologies</a>
+        <a href="#">Future of IT</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
